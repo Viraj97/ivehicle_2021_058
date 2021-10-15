ffe43cdfd35c8b22e758e149ed49675096875bd4
Nilu <niluhz@gmail.com>
2021-10-16 00:18:20 +0530

    Added validate_simple_vehicle_form in actions

--- actions/actions.py
+++ actions/actions.py
@@ -5,17 +5,17 @@
 # from rasa_sdk.executor import CollectingDispatcher
 #
 #
-# class ActionHelloWorld(Action):
-#
+# class ActionGreetUser(Action):
+
 #     def name(self) -> Text:
-#         return "action_hello_world"
-#
+#         return "action_greet_user"
+
 #     def run(self, dispatcher: CollectingDispatcher,
 #             tracker: Tracker,
 #             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
+
+#         dispatcher.utter_message(text="Hello How can I help you sir!")
+
 #         return []
 
 from typing import Any, Text, Dict, List
@@ -27,7 +27,8 @@ from rasa_sdk.types import DomainDict
 from rasa_sdk.events import SlotSet
 from rasa_sdk.forms import FormAction
 
-
+ALLOWED_VEHICLE_CON=["used","brand new","new","old","Used Vehicles","Brand New Vehicles"]
+ALLOWED_FUEL_TYPES = ["Petrol", "Diesel"]
 
 class ActionHelloWorld(FormAction):
 
@@ -51,4 +52,38 @@ class ActionHelloWorld(FormAction):
         return []
      
 
+## New Actions Added
 
+class ValidateSimplePizzaForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_simple_vehicle_form"
+
+    def validate_vehicle_con(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `vehicle_con` value."""
+
+        if slot_value.lower() not in ALLOWED_VEHICLE_CON:
+            dispatcher.utter_message(text=f"We only accept : Used Vehicles/Brand New Vehicles.")
+            return {"vehicle_con": None}
+        dispatcher.utter_message(text=f"OK! You want to have a {slot_value} car ")
+        return {"vehicle_con": slot_value}
+
+    def validate_fuel(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `fuel` value."""
+
+        if slot_value not in ALLOWED_FUEL_TYPES:
+            dispatcher.utter_message(text=f"I don't recognize fuel type. We serve {'/'.join(ALLOWED_PIZZA_TYPES)}.")
+            return {"fuel": None}
+        dispatcher.utter_message(text=f"OK! You want to have a {slot_value} car.")
+        return {"fuel": slot_value}
